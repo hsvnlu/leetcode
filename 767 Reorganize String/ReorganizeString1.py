ee7e3997ee37e9e140a1311f9cef8e195e5e08e1
hsvnlu <36412577+hsvnlu@users.noreply.github.com>
2019-02-16 07:01:02 -0800

    Update ReorganizeString1.py

--- 767 Reorganize String/ReorganizeString1.py
+++ 767 Reorganize String/ReorganizeString1.py
@@ -1,15 +1,17 @@
+"""
+16.02.2019
+hsvnlu
+time: 52ms
+space: 6.4MB
+Given a string S, check if the letters can be rearranged so that two characters that are adjacent to each other are not the same.
+If possible, output any possible result.  If not possible, return the empty string.
+"""
 class Solution:
     def reorganizeString(self, S):
-        """
-        16.02.2019
-        hsvnlu
-        time: 52ms
-        space: 6.4MB
-        :type S: str
+	"""
+	:type S: str
         :rtype: str
-        Given a string S, check if the letters can be rearranged so that two characters that are adjacent to each other are not the same.
-		If possible, output any possible result.  If not possible, return the empty string.
-        """
+	"""
         a = {i: S.count(i) for i in S}
         a = sorted(a.items(), key = lambda item:item[1], reverse = True)
         if a[0][1] > int((len(S) + 1)/2): return ""
