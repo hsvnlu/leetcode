3ac77c51cd784af6470c9159a37a1a02d617d875
hsvnlu <36412577+hsvnlu@users.noreply.github.com>
2019-02-25 23:46:55 +0800

    Create Delete Node in a Linked List.py

---
+++ 237 easy Delete Node in a Linked List/Delete Node in a Linked List.py
@@ -0,0 +1,30 @@
+'''
+2019/2/25
+hsvnlu
+runtime: 44ms 64.15%
+memory: 13.5MB
+'''
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+'''        
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+'''
